d4357500b5940505ca54d20745c7e2b1a67478e7
Xefise <Xefise@gmail.com>
2019-09-27 22:26:16 +0300

    Add resouecr dictionary and set it

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,15 @@ namespace SEW
         public MainWindow()
         {
             InitializeComponent();
+            UpdateTheme();
+        }
+
+        public static void UpdateTheme()
+        {
+            //if (!Properties.Settings.Default.DarkTheme) Theme = "Light"; else Theme = "Dark";
+            ResourceDictionary resourceDict = Application.LoadComponent(new Uri($"Themes/Dark.xaml", UriKind.Relative)) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
         }
     }
 }
