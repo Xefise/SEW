5698bc57874bbacce086eb8f295a2b09367e1e1e
Xefise <Xefise@gmail.com>
2019-10-03 19:53:46 +0300

    Add public variables

--- Objects/Category.cs
+++ Objects/Category.cs
@@ -11,5 +11,30 @@ namespace SEW
         private long _ID;
         private string _Name;
         private List<Word> _words;
+
+        public long ID
+        {
+            get { return _ID; }
+            set
+            {
+                _ID = value;
+            }
+        }
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+            }
+        }
+        public List<Word> words
+        {
+            get { return _words; }
+            set
+            {
+                _words = value;
+            }
+        }
     }
 }
--- Objects/Word.cs
+++ Objects/Word.cs
@@ -11,5 +11,62 @@ namespace SEW
         private byte _Review;
         private Category _category;
         private string _Transcription;
+
+        public long ID
+        {
+            get { return _ID; }
+            set
+            {
+                _ID = value;
+            }
+        }
+        public string InEnglish
+        {
+            get { return _InEnglish; }
+            set
+            {
+                _InEnglish = value;
+            }
+        }
+        public string InRussian
+        {
+            get { return _InRussian; }
+            set
+            {
+                _InRussian = value;
+            }
+        }
+        public DateTime CanBeDisplayedAt
+        {
+            get { return _CanBeDisplayedAt; }
+            set
+            {
+                _CanBeDisplayedAt = value;
+            }
+        }
+        public byte Review
+        {
+            get { return _Review; }
+            set
+            {
+                _Review = value;
+            }
+        }
+        public Category category
+        {
+            get { return _category; }
+            set
+            {
+                _category = value;
+            }
+        }
+        public string Transcription
+        {
+            get { return _Transcription; }
+            set
+            {
+                _Transcription = value;
+            }
+        }
     }
 }
