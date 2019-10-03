b98f299df403ea15bf7983f7f9fa4f21819a5bd0
Xefise <Xefise@gmail.com>
2019-10-03 20:18:46 +0300

    Add class 'Day'

---
+++ Objects/Day.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SEW
+{
+    public class Day
+    {
+        private DateTime _Data;
+        private int _AlReadyKnown;
+        private int _NewWords; //memorized
+        private int _Reviewed;
+        private int _Learned;
+        private int _Score; // _AlReadyKnown + _NewWords + _ReViewed + _Learned
+
+        public DateTime Data
+        {
+            get { return _Data; }
+            set
+            {
+                _Data = value;
+            }
+        }
+        public int AlReadyKnown
+        {
+            get { return _AlReadyKnown; }
+            set
+            {
+                _AlReadyKnown = value;
+            }
+        }
+        public int NewWords
+        {
+            get { return _NewWords; }
+            set
+            {
+                _NewWords = value;
+            }
+        }
+        public int Reviewed
+        {
+            get { return _Reviewed; }
+            set
+            {
+                _Reviewed = value;
+            }
+        }
+        public int Learned
+        {
+            get { return _Learned; }
+            set
+            {
+                _Learned = value;
+            }
+        }
+        public int Score
+        {
+            get { return _Score; }
+            set
+            {
+                _Score = value;
+            }
+        }
+
+    }
+}
