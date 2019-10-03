9a9d264273d2ae805c8169bc05682410f9e89420
Xefise <Xefise@gmail.com>
2019-10-03 19:44:45 +0300

    Add few objects

---
+++ Objects/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEW
+{
+    public class Category
+    {
+        private long _ID;
+        private string _Name;
+        private List<Word> _words;
+    }
+}
---
+++ Objects/Word.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SEW
+{
+    public class Word
+    {
+        private long _ID;
+        private string _InEnglish;
+        private string _InRussian;
+        private DateTime _CanBeDisplayedAt;
+        private byte _Review;
+        private Category _category;
+        private string _Transcription;
+    }
+}
