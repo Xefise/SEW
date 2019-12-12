8662b0aba37482c3ca6988a10a6880edfe9dcdb0
Xefise <Xefise@gmail.com>
2019-12-13 00:38:31 +0300

    Try to add commands

---
+++ Command/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SEW.Command
+{
+    public interface ICommand
+    {
+        event EventHandler CanExecuteChanged;
+        void Execute(object parameter);
+        bool CanExecute(object parameter);
+    }
+}
